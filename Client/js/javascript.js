d47447826280ef9cacfaf89f138e7b9f489fe689
NakunMinh <nakunminh@gmail.com>
2016-10-24 18:47:28 +0700

    Load dữ liệu trang tìm kiếm

--- Client/js/javascript.js
+++ Client/js/javascript.js
@@ -11,8 +11,11 @@ app.config(function($routeProvider){
 	.when('/motchieu', {
 		templateUrl: 'motchieu.html'
 	})
-	.when('/minhnhi', {
-		templateUrl: 'minhnhi.html'
+	.when('/resultkhuhoi', {
+		templateUrl: 'result_khuhoi.html'
+	})
+	.when('/resultmotchieu',{
+		templateUrl: 'result_motchieu.html'
 	})
 	.otherwise({
 		redirectTo: '/'
@@ -37,11 +40,84 @@ app.controller('Abc', function($scope, $http, $location) {
 	};
 
 	$scope.khuhoitimkiem = function(){
-		console.log("here");
-		var a = $('#abc').find(":selected").text();
-		var b = $('.nav-tabs .active').text();
-		console.log(b);
-		$scope.abc = "Minh aaa";
-		window.location.hash = '#/minhnhi';
+		$scope.ngaydi = $('#ngaydi').val();
+		$scope.ngayve = $('#ngayve').val();
+		var noidi = $('#noidi').find(":selected").text();
+		var noiden = $('#noiden').find(":selected").text();
+		$http.get('http://localhost:3333/api/resultkhuhoi?noidi=' + noidi.toString() + '&noiden=' + noiden.toString())
+		.then(function(response){
+			console.log(response.data);
+			$scope.resultkhuhoidi = response.data;
+		});
+		$http.get('http://localhost:3333/api/resultkhuhoi?noidi=' + noiden.toString() + '&noiden=' + noidi.toString())
+		.then(function(response){
+			console.log(response.data);
+			$scope.resultkhuhoive = response.data;
+		});
+		window.location.hash = '#/resultkhuhoi';
+	};
+
+	$scope.checkboxchange = function(){
+		var tongtien = 0;
+		var selected1 = [];
+		var selected2 = [];
+		var table1 = document.getElementById('khuhoidi');
+		var table2 = document.getElementById('khuhoive');
+		$('#khuhoidi input:checked').each(function() {
+    		selected1.push($(this).attr('name'));
+		});
+		$('#khuhoive input:checked').each(function() {
+    		selected2.push($(this).attr('name'));
+		});
+		for (var i = 0; i < selected1.length; i++){
+			tongtien += parseInt(table1.rows[parseInt(selected1[i]) + 1].cells[5].innerHTML, 10);
+		}
+		for (var i = 0; i < selected2.length; i++){
+			tongtien += parseInt(table2.rows[parseInt(selected2[i]) + 1].cells[5].innerHTML, 10);
+		}
+		$scope.tongtien = tongtien;
+	}
+	$scope.continue = function(){
+		var tongtien = 0;
+		var selected1 = [];
+		var selected2 = [];
+		var table1 = document.getElementById('khuhoidi');
+		var table2 = document.getElementById('khuhoive');
+		$('#khuhoidi input:checked').each(function() {
+    		selected1.push($(this).attr('name'));
+		});
+		$('#khuhoive input:checked').each(function() {
+    		selected2.push($(this).attr('name'));
+		});
+		for (var i = 0; i < selected1.length; i++){
+			tongtien += parseInt(table1.rows[parseInt(selected1[i]) + 1].cells[5].innerHTML, 10);
+		}
+		for (var i = 0; i < selected2.length; i++){
+			tongtien += parseInt(table2.rows[parseInt(selected2[i]) + 1].cells[5].innerHTML, 10);
+		}
+		$scope.tongtien = tongtien;
+		$scope.luutru = [];
+		for (var i = 0; i < selected1.length; i++){
+			var Obj = {
+				gio: table1.rows[parseInt(selected1[i]) + 1].cells[1].innerHTML,
+				hang: table1.rows[parseInt(selected1[i]) + 1].cells[2].innerHTML,
+				mucgia: table1.rows[parseInt(selected1[i]) + 1].cells[3].innerHTML,
+				soluongghe: parseInt(table1.rows[parseInt(selected1[i]) + 1].cells[4].innerHTML),
+				giaban: parseInt(table1.rows[parseInt(selected1[i]) + 1].cells[5].innerHTML)
+			};
+			$scope.luutru.push(Obj);
+		}
+		for (var i = 0; i < selected2.length; i++){
+			var Obj = {
+				gio: table2.rows[parseInt(selected2[i]) + 1].cells[1].innerHTML,
+				hang: table2.rows[parseInt(selected2[i]) + 1].cells[2].innerHTML,
+				mucgia: table2.rows[parseInt(selected2[i]) + 1].cells[3].innerHTML,
+				soluongghe: parseInt(table2.rows[parseInt(selected2[i]) + 1].cells[4].innerHTML),
+				giaban: parseInt(table2.rows[parseInt(selected2[i]) + 1].cells[5].innerHTML)
+			};
+			$scope.luutru.push(Obj);
+		}
+		console.log($scope.luutru[0].giaban);
+		//khoi tao ma dat cho - trang thai 0
 	};
 });
