35abbbb9113746ad0c8a57f23ca11dae6349a088
NakunMinh <nakunminh@gmail.com>
2016-10-25 17:51:06 +0700

    Update client

--- Client/js/javascript.js
+++ Client/js/javascript.js
@@ -139,19 +139,68 @@ app.controller('Abc', function($scope, $http, $location) {
 			$scope.luutru.push(Obj);
 		}
 		//khoi tao ma dat cho - trang thai 0 MaDatCho|ThoiGianDat|TongTien|TrangThai
-		//tra ve ma dat cho
+		var dt = new Date();
+		var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+		$scope.thoigiandat = time;
+		var O = {
+			Thoigiandatcho: time,
+			Tongtien: $scope.tongtien,
+			Trangthai: 0
+		}
+		$http.post('http://localhost:3000/api/booking', O)
+		.then(function(response){
+			console.log("post" + time + response.data);
+			//$scope.resultmotchieu = response.data;
+			$http.get('http://localhost:3000/api/booking?Thoigiandatcho=' + time + '&Tongtien=' + $scope.tongtien + '&Trangthai=0')
+			.then(function(response){
+				console.log("get" + time + response.data);
+				$scope.madatcho = response.data;
+			});
+		});
+		
 		window.location.hash = '/comfirm';
 	};
 
 	$scope.them = function(){
 		var ten = $('#ten').val();
 		var hochieu = $('#hochieu').val();
+		var madatcho = $('#madatcho').val();
+		var O = {
+			Madatcho: madatcho,
+			Ten: ten,
+			Hochieu: hochieu
+		}
 		//cap nhat vao bang MaDatCho|Ten|HoChieu
+		$http.post('http://localhost:3000/api/passenger', O)
+			.then(function(response){
+		});
 	}
 
 	$scope.hoantat = function(){
 		//them du lieu vao bang MaDatCho|Ngay|Hang|Mucgia
+		var tbcomfirm = document.getElementById('tbcomfirm');
+		console.log(tbcomfirm.rows.length);
+		for (var i = 1; i < tbcomfirm.rows.length - 1; i++){
+			var A = {
+				Madatcho: $('#madatcho').val(),
+				Gio: tbcomfirm.rows[i + 1].cells[0],
+				Hang: tbcomfirm.rows[i + 1].cells[1],
+				Mucgia: tbcomfirm.rows[i + 1].cells[2]
+			};
+			$http.post('http://localhost:3000/api/flightdetail', A)
+			.then(function(response){
+			});
+		}
 		//cap nhat lai trang thai 1 MaDatCho|ThoiGianDat|TongTien|TrangThai
+		var Ob = {
+			Trangthai: 1,
+			Ma: $('#madatcho').val(),
+			Thoigiandatcho: $('#thoigiandat').val(),
+			Tongtien: $('#tongtien').val()
+		}
+		$http.put('http://localhost:3000/api/booking', Ob)
+			.then(function(response){
+		});
 		window.location.hash = '/success';
 	};
 });
